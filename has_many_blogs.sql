97b2917b5606ec0c6403d1bc33c7d3846471fa91
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-07-26 21:49:29 -1000

    create users tbl

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -4,12 +4,24 @@ DROP USER IF EXISTS has_many_user;
 CREATE USER has_many_user;
 CREATE DATABASE has_many_blogs WITH OWNER has_many_user;
 
+\c has_many_blogs
+
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS posts;
 DROP TABLE IF EXISTS comments;
 
 
+CREATE TABLE users (
+  id SERIAL NOT NULL PRIMARY KEY,
+  username VARCHAR(90) NOT NULL,
+  first_name VARCHAR(90) DEFAULT NULL,
+  last_name VARCHAR(90) DEFAULT NULL,
+  created_at TIMESTAMP with time zone NOT NULL DEFAULT NOW(),
+  update_at TIMESTAMP with time zone NOT NULL DEFAULT NOW()
+  );
 
+\d users
 
+\c dorybaga
 
 
