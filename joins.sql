8418b4e68c893cfd10791d49a6923ae4ddda0194
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-07-27 19:30:02 -1000

    corrected #3-#5

--- joins.sql
+++ joins.sql
@@ -3,22 +3,29 @@ SELECT * FROM users;
 
 --Create a query to get all fields from the posts table where the user_id is 100
 SELECT * FROM posts
-WHERE user_id = 100;
+WHERE users_id = 100;
 
 --Create a query to get all posts fields, the user's first name, and the user's last name, from the posts table where the user's id is 200
 SELECT posts.*, users.first_name, users.last_name
 FROM posts INNER JOIN users
-ON users_id = 200;
+ON posts.users_id = users.id
+WHERE users.id = 200;
 
 --Create a query to get all posts fields, and the user's username, from the posts table where the user's first name is 'Norene' and the user's last_name is 'Schmitt'
 SELECT posts.*, users.username
 FROM posts INNER JOIN users
-ON users.first_name = 'Norene' AND users.last_name = 'Schmitt';
+ON posts.users_id = users.id
+WHERE users.first_name = 'Norene' AND users.last_name = 'Schmitt';
 
 --Create a query to get usernames from the users table where the user has created a post after January 1, 2015
-SELECT username FROM users WHERE users.created_at >= '2015-01-01';
+SELECT username FROM users
+INNER JOIN posts
+ON posts.users_id = users.id
+WHERE posts.created_at >= '2015-01-01';
 
 --Create a query to get the post title, post content, and user's username where the user who created the post joined before January 1, 2015
+
+
 
 --Create a query to get the all rows in the comments table, showing post title (aliased as 'Post Title'), and the all the comment's fields
 
